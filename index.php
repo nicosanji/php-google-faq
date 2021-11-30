0ffa5b19e3d64090fbc5a68884c9dd5562ec2f4e
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-11-30 13:59:07 +0100

    START

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Google FAQ</title>
+  <!-- link bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <!-- link css -->
+  <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+
+  <header>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <img src="img/Google-Logo.png" alt="google-logo-img">
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <main>
+
+  </main>
+
+  <footer>
+
+  </footer>
+</body>
+
+</html>
